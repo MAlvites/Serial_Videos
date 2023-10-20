5e1ddbdd5e72e037db93d2106abef3e45843800d
Marcelo <malvites1602@gmail.com>
2023-10-19 22:20:55 -0500

    VRI_bottom modified

--- Videos/VRI_bottom/scripts/config_xml.js
+++ Videos/VRI_bottom/scripts/config_xml.js
@@ -2,7 +2,7 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description dc:date="2023-10-19 03:23:41 p. m." dc:source="Camtasia,21.0.19,esn" dc:title="VRI_bottom" tscDM:firstFrame="VRI_bottom_Primer_Fotograma.png" tscDM:originId="6BCB3DAF-3EA7-4980-8181-33B0EBCD52E0" tscDM:project="Video_VRI">\
+      <rdf:Description dc:date="2023-10-19 10:20:17 p. m." dc:source="Camtasia,21.0.19,esn" dc:title="VRI_bottom" tscDM:firstFrame="VRI_bottom_Primer_Fotograma.png" tscDM:originId="6BCB3DAF-3EA7-4980-8181-33B0EBCD52E0" tscDM:project="Video_VRI">\
          <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="140300"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="320" stDim:w="480"/>\
          <tsc:langName>\
